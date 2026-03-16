59bba19555efeccfd376f65c372febb928a4d096
songyejin <songyejin0923@dongyang.ac.kr>
2026-03-16 11:17:35 +0900

    Bye class

---
+++ src/main/java/Bye.java
@@ -0,0 +1,17 @@
+public class Bye {
+
+    public static void main(String[] args) {
+        String name = "yejin";
+        System.out.println("name = " + name);
+
+        int age = 21;
+
+        System.out.println("I am " + name + ", " + age + " years old.");
+        System.out.printf("I am %s, %d years old.\n", name, age);
+        System.out.print("hello ");
+        System.out.print("world");
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+    }
+
+}
